71469bb0bc7f9331b3eb2bc58dad0f6a944de7e5
S.SHASHIKANTH <110800354+SHASHIKANTHSURABHI@users.noreply.github.com>
2024-03-07 22:01:35 +0530

    Create histogram equalisation and comparing.py

---
+++ histogram equalisation and comparing.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image = cv2.imread('C:/Users/Shashikanth/OneDrive/Pictures/IMG_20220704_170818.png', cv2.IMREAD_GRAYSCALE)
+equalized_image = cv2.equalizeHist(image)
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.imshow(equalized_image, cmap='gray')
+plt.title('Equalized Image')
+plt.axis('off')
+plt.show()
