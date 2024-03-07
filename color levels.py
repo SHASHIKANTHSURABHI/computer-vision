36fe3afdd6a107c2a4c8915652b3b19a8dfb3f57
S.SHASHIKANTH <110800354+SHASHIKANTHSURABHI@users.noreply.github.com>
2024-03-07 21:59:04 +0530

    Create color levels.py

---
+++ color levels.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+def analyze_histogram(image_path):
+    image = cv2.imread(image_path)
+    b, g, r = cv2.split(image)
+    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
+    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
+    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+    plt.figure(figsize=(10, 5))
+    plt.subplot(3, 1, 1)
+    plt.plot(hist_b, color='b')
+    plt.title('Blue Channel Histogram')
+    plt.xlim([0, 256])
+    plt.subplot(3, 1, 2)
+    plt.plot(hist_g, color='g')
+    plt.title('Green Channel Histogram')
+    plt.xlim([0, 256])
+    plt.subplot(3, 1, 3)
+    plt.plot(hist_r, color='r')
+    plt.title('Red Channel Histogram')
+    plt.xlim([0, 256])
+    plt.tight_layout()
+    plt.show()
+analyze_histogram('C:/Users/Shashikanth/OneDrive/Pictures/IMG_20220704_170818.png')
